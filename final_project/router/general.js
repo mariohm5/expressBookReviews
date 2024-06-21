f020574a3559d248893358323ef0a348792557b6
mariohm5 <mariohm5>
2024-06-21 01:24:14 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,9 +17,12 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let book = books[req.params.isbn];
+  if(book){
+    res.send(JSON.stringify(book, null, 4));
+  }
+  res.status(404).json({message: `The book with ISBN ${req.params.isbn} was not found.`});
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
