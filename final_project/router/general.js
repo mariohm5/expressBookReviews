f7bd5f3f3f1f5659a167a0ad8253f4cf24082aa3
mariohm5 <mariohm5>
2024-06-21 02:01:59 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,16 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Function to filter by book detail (author, title)
+const filterBooksByDetail = (detail, value) => {
+    let results = [];  
+    for (let [isbn, book] of Object.entries(books)) {
+      if(book[detail] == value){
+        results.push(book);
+      }
+    }
+    return results;
+};
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -23,11 +33,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
   }
   res.status(404).json({message: `The book with ISBN ${req.params.isbn} was not found.`});
 });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(filterBooksByDetail("author", req.params.author)));
 });
 
 // Get all books based on title
